839821550d4ece2dae188c11b8f9ce19c1e759b9
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-02-25 21:37:43 +0100

    test element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
+    <el-button bg text type="primary">test</el-button>
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
