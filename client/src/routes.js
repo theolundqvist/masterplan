edf2694eeb363aa217f6e4e6219c63128a33b97f
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-03-01 18:31:55 +0100

    add empty components and routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,8 @@
 import App from "./App.vue";
+import Welcome from "./components/Welcome.vue";
 import ChooseProgram from "./components/ChooseProgram.vue";
+import ChooseYear from "./components/ChooseYear.vue";
+import Overview from "./components/Overview.vue";
 
 // 2. Define some routes
 // Each route should map to a component.
@@ -8,6 +11,11 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
-  routes: [{ path: "/", component: ChooseProgram }], // short for `routes: routes`
+  routes: [
+    { path: "/", component: Welcome },
+    { path: "/choose-program", component: ChooseProgram },
+    { path: "/choose-year", component: ChooseYear },
+    { path: "/overview", component: Overview },
+  ], // short for `routes: routes`
 });
 export { router };
