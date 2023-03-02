b778481f86fa8d670826612b69f8ca32ed9b4af9
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-03-02 14:48:01 +0100

    stor knapp

--- client/src/components/Welcome.vue
+++ client/src/components/Welcome.vue
@@ -1,18 +1,22 @@
 <script setup>
-
- function say(message) {
-    alert(message);
-  }
+function say(message) {
+  alert(message);
+}
 </script>
 <template>
-    <h1>Create your Master Plan</h1>
+  <h1>Create your Master Plan</h1>
 
   <el-row class="mb-4">
-    <el-button type="primary" round @click="say('Hello')">Click me</el-button>
-</el-row>
+    <el-button class="stor" type="primary" round @click="say('Hello')"
+      >Click me</el-button
+    >
+  </el-row>
 </template>
 
 <style scoped>
+.stor {
+  transform: scale(2);
+}
 /* .el-button{ 
     padding: 1em;
     height: 10em;
