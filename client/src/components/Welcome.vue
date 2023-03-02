e4ba711d45fdd5a4a8963edf7cb428fa9b4e5631
Rebecca Liss <rebeccaliss97@gmail.com>
2023-03-02 11:11:11 +0100

    add button in welcome component

--- client/src/components/Welcome.vue
+++ client/src/components/Welcome.vue
@@ -1,2 +1,19 @@
-<script setup></script>
-<template></template>
+<script setup>
+
+ function say(message) {
+    alert(message);
+  }
+</script>
+<template>
+    <h1>Create your Master Plan</h1>
+    <el-button type="primary" circle @click="say('Hello')"/>
+    
+</template>
+
+<style scoped>
+.el-button{
+    padding: 1em;
+    height: 10em;
+    width: 20em;
+}
+</style>
