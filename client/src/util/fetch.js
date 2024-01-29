48f9dbcb95bed9574fce285fade3236a8c9fe1a8
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-01-29 10:03:02 +0100

    fix duplicate courses

--- client/src/util/fetch.js
+++ client/src/util/fetch.js
@@ -12,7 +12,7 @@ function safeFetch(url) {
 
 function fetchPrograms() {
   return safeFetch(
-    "https://api.lth.lu.se/lot/courses/programmes?kull=false"
+    "https://api.lth.lu.se/lot/courses/programmes?kull=false",
   ).then((data) => {
     return data
       .map((x) => ({
@@ -26,7 +26,7 @@ function fetchPrograms() {
 function fetchYears(program) {
   if (!program) return Promise.reject(new Error("invalid arguments"));
   return safeFetch(
-    `https://api.lth.lu.se/lot/courses/academic-years?programmeCode=${program}&includePreliminary=true`
+    `https://api.lth.lu.se/lot/courses/academic-years?programmeCode=${program}&includePreliminary=true`,
   ).then((data) => {
     return data
       .map((x) => ({
@@ -42,20 +42,25 @@ function fetchYears(program) {
 function fetchCourses(program, year) {
   if (!program || !year) return Promise.reject(new Error("invalid arguments"));
   return safeFetch(
-    `https://api.lth.lu.se/lot/courses?programmeCode=${program}&academicYearId=${year}`
+    `https://api.lth.lu.se/lot/courses?programmeCode=${program}&academicYearId=${year}`,
   ).then((data) => {
-    return data.map((x) => new Course(x)).sort((a, b) => a.compare(b));
+    data = data
+      .map((x) => new Course(x))
+      .sort((a, b) => a.compare(b));
+      const coursecodes = data.map(x => x.courseCode);
+    return data.filter((x, index) => coursecodes.indexOf(x.courseCode) == index);
   });
 }
 
 function fetchTenta(program, year) {
   if (!program || !year) return Promise.reject(new Error("invalid arguments"));
-  console.log(__APP_ENV__.SERVER_URL)
+  console.log(__APP_ENV__.SERVER_URL);
   return safeFetch(
-    `${__APP_ENV__.SERVER_URL || "http://localhost:1234"}/tenta?course=${program}&year=${year}`
+    `${__APP_ENV__.SERVER_URL || "http://localhost:1234"
+    }/tenta?course=${program}&year=${year}`,
   ).then((data) => {
     return data;
   });
 }
 
-export { safeFetch, fetchPrograms, fetchYears, fetchCourses, fetchTenta };
+export { fetchCourses, fetchPrograms, fetchTenta, fetchYears, safeFetch };
